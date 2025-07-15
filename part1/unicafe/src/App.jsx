111228e090491f475f81f135d2678218fe196aa7
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-07-16 00:30:36 +0530

    part1 unicafe till ex 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <>
-      <p>{text}: {value}</p>
-    </>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -21,14 +22,16 @@ const Statistics = ({ total, good, neutral, bad, average, positive }) => {
       {total === 0 ? (
         <p>No feedback given! :(</p>
       ) : (
-        <>
-          <StatisticLine text="Good" value={good} />
-          <StatisticLine text="Neutral" value={neutral} />
-          <StatisticLine text="Bad" value={bad} />
-          <StatisticLine text="Total" value={total} />
-          <StatisticLine text="Average Score" value={average} />
-          <StatisticLine text="Positive Feedback" value={positive + " %"} />
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text="Good" value={good} />
+            <StatisticLine text="Neutral" value={neutral} />
+            <StatisticLine text="Bad" value={bad} />
+            <StatisticLine text="Total" value={total} />
+            <StatisticLine text="Average Score" value={average} />
+            <StatisticLine text="Positive Feedback" value={positive + " %"} />
+          </tbody>
+        </table>
       )}
     </>
   )
