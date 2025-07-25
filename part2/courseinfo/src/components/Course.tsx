b337983cde1faf23f30f24841b2a43b5e3202a7b
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-07-26 00:06:22 +0530

    part 2 -> ex 2.2 done

--- part2/courseinfo/src/components/Course.tsx
+++ part2/courseinfo/src/components/Course.tsx
@@ -22,11 +22,19 @@ const Content = ({ parts }) => {
     )
 }
 
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return (
+        <p><strong>Total exercises: {total}</strong></p>
+    )
+}
+
 const Course = ({ course }) => {
     return (
         <div>
             <Header course={course} />
             <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
