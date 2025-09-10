598036cd297a0c2f80b97d91a26b24733ba8d7b5
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-09-10 22:00:14 +0530

    part3 -> ex 3.11

--- part2/phonebook/src/services/personService.js
+++ part2/phonebook/src/services/personService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://phonebook-backend-8kiy.onrender.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
     return axios.get(baseUrl);
