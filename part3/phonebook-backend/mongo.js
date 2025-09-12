4b612f142d9ef09f25af0bf5a6c32cda7d41b10c
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-09-12 22:55:12 +0530

    part3 -> ex 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide a password')
+    process.exit(1)
+}
+
+// password
+const password = process.argv[2]
+
+const url = `mongodb+srv://harshilshrma:${password}@fullstackopen.6c8kv3o.mongodb.net/phonebook?retryWrites=true&w=majority&appName=FullStackOpen`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+// creating the person schema and model
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+const Person = mongoose.model('Person', personSchema)
+
+// adding a person if 5 arguments
+if (process.argv.length === 5) {
+    const givenName = process.argv[3]
+    const givenNumber = process.argv[4]
+
+    const person = new Person({
+        name: givenName,
+        number: givenNumber
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${givenName} | Number: ${givenNumber} to phonebook!`)
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 3) { // for listing all phonebook entries
+    console.log('Phonebook:')
+    Person
+        .find({})
+        .then(result => {
+            result.forEach(person => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close()
+        })
+} else {
+    console.log('Invalid number of arguments!')
+    process.exit(1)
+}
+
