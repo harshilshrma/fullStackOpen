b8d607def32d0b97448cf673c1d3c46bd4b084e1
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-05 14:59:37 +0530

    minor change in returning returnedObject of personSchema

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -36,6 +36,7 @@ personSchema.set('toJSON', {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
+        return returnedObject
     }
 })
 
