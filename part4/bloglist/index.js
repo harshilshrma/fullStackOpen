10d21875583bd6b3d5c7d3f796b2e68c8d893e77
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-05 15:34:17 +0530

    4.1

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 
@@ -10,12 +11,30 @@ const blogSchema = mongoose.Schema({
     likes: Number,
 })
 
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        return returnedObject
+    }
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
+    .then(() => {
+        console.log('Connected to MongoDB!')
+    }).catch(error => {
+        console.log('Error connecting to MongoDB:', error);
+    })
 
 app.use(express.json())
+
+app.get('/', (request, response, next) => {
+    response.send('<h1>Henlo! Backend started!</h1>')
+})
 
 app.get('/api/blogs', (request, response) => {
     Blog.find({}).then((blogs) => {
@@ -31,7 +50,12 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 
+const unknownEndpoint = (request, response) => {
+    return response.status(404).send({ error: "unknown endpoint" })
+}
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
