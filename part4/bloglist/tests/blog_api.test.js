91fef928e3430f40461cda92550e8cce35b89a54
Harshil Sharma <harshilsharma.dev@gmail.com>
2026-06-05 09:32:27 +0530

    4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -49,7 +49,24 @@ test('the unique identifier property of the blog posts is named id', async () =>
     const response = await api.get('/api/blogs')
     const firstBlog = response.body[0];
 
-    assert.strictEqual(firstBlog.hasOwnProperty("id"), true);
+    assert(firstBlog.hasOwnProperty("id"));
+})
+
+test('making an HTTP POST request to the /api/blogs URL successfully creates a new blog post', async() => {
+    const testBlog = {
+        title: "test blog",
+        author: "abvdd",
+        url: ".com",
+        likes: 3
+    }
+
+    await api.post('/api/blogs').send(testBlog);
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+    const blogsTitleArray = response.body.map(blog => blog.title)
+    assert(blogsTitleArray.includes("test blog"))
 })
 
 after(async () => {
