f91969a3c41888b570ce08ba02cf284e268cf455
Harshil Sharma <harshilsharma.dev@gmail.com>
2026-08-11 14:37:31 +0530

    5.3: Blog List Frontend, step 3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,13 +2,18 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import './App.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [likes, setLikes] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -23,7 +28,7 @@ const App = () => {
       setUser(user)
     }
   }, [])
-  
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
@@ -34,14 +39,15 @@ const App = () => {
       )
       setUsername('')
       setPassword('')
-    } catch {
-      setErrorMessage('Wrong Credentials!')
+      setErrorMessage('')
+    } catch (error) {
+      setErrorMessage(error.response.data.error)
       setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+        setErrorMessage('')
+      }, 3500)
     }
   }
-  
+
   const handleLogout = (event) => {
     event.preventDefault()
     window.localStorage.removeItem('loggedUser')
@@ -53,6 +59,7 @@ const App = () => {
   const loginForm = () => {
     return (
       <form onSubmit={handleLogin}>
+        <h2>Login to the Blogs application!</h2>
         <div>
           <label>
             Username:
@@ -78,21 +85,86 @@ const App = () => {
     )
   }
 
+  const addNewBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const response = await blogService.addBlog({ title, author, url, likes }, user.token)
+      setBlogs(blogs.concat(response))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setLikes('')
+      setErrorMessage('')
+    } catch (error) {
+      setErrorMessage(error.response.data.error)
+      setTimeout(() => {
+        setErrorMessage('')
+      }, 5000)
+    }
+  }
+
+  const createNewBlogForm = () => {
+    return (
+      <form onSubmit={addNewBlog} className='create-new-blog'>
+        <h2>Create New Blog</h2>
+        <label>
+          Title*:
+          <input
+            type="text"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </label>
+        <label>
+          Author:
+          <input
+            type="text"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </label>
+        <label>
+          URL*:
+          <input
+            type="text"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </label>
+        <label>
+          Likes:
+          <input
+            type="number"
+            value={likes}
+            onChange={({ target }) => setLikes(target.value)}
+          />
+        </label>
+        <button type="submit">Create</button>
+      </form>
+    )
+  }
+
   return (
     <div>
       {!user && loginForm()}
       {user && (
-        <>
-          <h2>Hi {user.name}, you are logged in!</h2>
-          <button onClick={handleLogout}>Logout</button>
+        <div className='container'>
+          <h2 className='title'>Blogs</h2>
+          <div className='user-login'>
+            <h3>Hi {user.name}, you are logged in!</h3>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+          {createNewBlogForm()}
           <div>
-            <h2>blogs</h2>
             {blogs.map(blog =>
               <Blog key={blog.id} blog={blog} />
             )}
           </div>
-        </>
+        </div>
       )}
+      {errorMessage &&
+        <div className='error-dialog'>{errorMessage}</div>
+      }
     </div>
   )
 }
