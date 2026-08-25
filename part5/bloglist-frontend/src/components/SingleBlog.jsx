b7a9295ab2ea70e5ad9b06d0d8694d8f1e7916dc
Harshil Sharma <harshilsharma.dev@gmail.com>
2026-08-25 14:46:21 +0530

    5.31: styled blogs, step 3

--- part5/bloglist-frontend/src/components/SingleBlog.jsx
+++ part5/bloglist-frontend/src/components/SingleBlog.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import { formatUrl } from '../utils'
+import { Button } from '@mui/material'
 
 const SingleBlog = ({ blogs, user, handleLike, handleRemoveBlog }) => {
     const blogId = useParams().id
@@ -23,19 +24,23 @@ const SingleBlog = ({ blogs, user, handleLike, handleRemoveBlog }) => {
     }
 
     return (
-        <div>
-            <h2>{singleBlog.title} – {singleBlog.author}</h2>
+        <div className='single-blog-container'>
+            <h2>{singleBlog.title}</h2>
+            <h3>by {singleBlog.author}</h3>
             <div className='blog-content'>
                 <a href={formatUrl(singleBlog.url)} target='_blank'>{singleBlog.url}</a>
+                {singleBlog.user && <span>Added by {singleBlog.user.name} ({singleBlog.user.username})</span>}
                 <div className='likes-container'>
-                    Likes: {singleBlog.likes}
-                    {user ? <button onClick={() => handleLike(singleBlog)}>like</button> : <p>(<a href='/login'>Log in</a> to like this blog!)</p>}
+                    {singleBlog.likes} likes
+                    {user
+                        ? <Button variant='contained' size='small' onClick={() => handleLike(singleBlog)}>like</Button>
+                        : <p>(<a href='/login'>Log in</a> to like this blog!)</p>
+                    }
+                    {showRemoveButton &&
+                        <Button variant='contained' size='small' color='error' className='blog-remove-button' onClick={() => handleRemoveBlogClick(singleBlog)}>Remove</Button>
+                    }
                 </div>
-                {singleBlog.user && <span>Created by {singleBlog.user.name} ({singleBlog.user.username})</span>}
             </div>
-            {showRemoveButton &&
-                <button className='blog-remove-button' onClick={() => handleRemoveBlogClick(singleBlog)}>Remove</button>
-            }
         </div>
     )
 }
